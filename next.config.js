cc5b4ad5bef11248590ce00c240848fa0fcf8a9a
Muhammed Jaseem Pallikkal <jaseemseems@gmail.com>
2022-09-17 17:56:59 +0530

    added basepath

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { unoptimized: true },
+  basePath: "/cirfl"
+  assetPrefix: "/cirfl"
 };
 
 module.exports = nextConfig;
