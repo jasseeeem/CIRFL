1fe9b641b0dbc0c05b53d587fe36123cef749ba0
Muhammed Jaseem Pallikkal <jaseemseems@gmail.com>
2022-09-17 18:09:35 +0530

    added basepath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isDev = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { unoptimized: true },
   basePath: "/CIRFL",
-  assetPrefix: "/CIRFL",
+  assetPrefix: "/CIRFL/",
 };
 
 module.exports = nextConfig;
